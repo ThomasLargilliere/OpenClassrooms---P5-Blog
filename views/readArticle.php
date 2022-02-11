d8461939d0632a9f62242b29fa40db1f6e8a6a46
Thomas <87136384+ThomasLargilliere@users.noreply.github.com>
2022-02-11 17:23:09 +0100

    Add files via upload

--- views/readArticle.php
+++ views/readArticle.php
@@ -36,16 +36,25 @@
 	<div class="col-10 offset-1">
 		<?php foreach ($commentaires as $commentaire){ ?>
 		<div class="col-12 p-1 mt-3">
-			<p>Ecris le : <?= $commentaire['formated_created_at']; ?> par <?= $commentaire['pseudo']; ?></p>
-			<p class="text-justify"><?=$commentaire['content']; ?></p>
-			
-			<?php
-				if ((isset($_SESSION['user']) && ($_SESSION['user'] == $commentaire['id_user'] || $is_admin == 1) )){ 
-			?>
-				<a href="index.php?action=comment&task=delete&id=<?=$commentaire['id_comment'];?>&id_article=<?= $article['id']; ?>" onclick="return confirm(`Êtes-vous sûr de vouloir supprimer ce commentaire ?`)" class="btn btn-danger mb-3">Supprimer</a>
-			<?php } ?>
-		
-		
+			<div class="d-flex">
+				<?php if ($commentaire['image']) { ?>
+				<div>
+					<img style="width:50px;height: 50px;" src="<?= $commentaire['image']; ?>">
+				</div>
+				<?php } ?>
+				<div class="<?php if ($commentaire['image']){ echo 'mx-2'; } ?>">
+					<p>Ecris le : <?= $commentaire['formated_created_at']; ?> par <?= $commentaire['pseudo']; ?></p>
+				</div>
+			</div>
+			<div class="mt-3">
+				<p class="text-justify"><?=$commentaire['content']; ?></p>
+				
+				<?php
+					if ((isset($_SESSION['user']) && ($_SESSION['user'] == $commentaire['id_user'] || $is_admin == 1) )){ 
+				?>
+					<a href="index.php?action=comment&task=delete&id=<?=$commentaire['id_comment'];?>&id_article=<?= $article['id']; ?>" onclick="return confirm(`Êtes-vous sûr de vouloir supprimer ce commentaire ?`)" class="btn btn-danger mb-3">Supprimer</a>
+				<?php } ?>
+			</div>
 		</div>
 		<?php } ?>
 	</div>
