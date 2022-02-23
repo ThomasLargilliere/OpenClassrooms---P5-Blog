c4fbd39367a7344066e7ef931d08276353e08013
Thomas <87136384+ThomasLargilliere@users.noreply.github.com>
2022-02-23 14:58:32 +0100

    Add files via upload

--- libraries/Database.php
+++ libraries/Database.php
@@ -9,7 +9,8 @@ class Database
 
         if (self::$instance === null)
         {
-            self::$instance = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '', [
+            require('env.php');
+            self::$instance = new PDO('mysql:host='.$host_bdd.';dbname='.$name_bdd.';charset=utf8', $user_bdd, $password_bdd, [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
